296d0da08a3a5e1ba0f17470ee25bdf735acf9e8
Isall-ux <tetroverse4@gmail.com>
2025-10-20 08:23:56 +0700

    tp-2

---
+++ basic_js/tp-2/index.js
@@ -0,0 +1,12 @@
+const nilai = 50;
+const name = 'mas eko';
+
+function lulus(){
+    if(nilai > 75){
+        console.log(`selamat ${name} maneh lulus euy, mahal mahal, nilai mane ${nilai}`)
+    }else{
+        console.log(`eleuh sia ${name} meni bodo bodo teing, naha bisa meunang nilai ${nilai} ai mane`)
+    }
+}
+
+lulus()
